196e9945025a0c8cebb181f314ab28be8bcf998d
leabastien <leabastien758@gmail.com>
2023-01-16 15:36:54 +0100

    thing ig

--- src/client/java/lea/playerknower/event/KeyInputHandler.java
+++ src/client/java/lea/playerknower/event/KeyInputHandler.java
@@ -3,14 +3,19 @@ package lea.playerknower.event;
 import lea.playerknower.PlayerKnower;
 import lea.playerknower.PlayerKnowerCommand;
 import lea.playerknower.networking.ModMessages;
+import lea.playerknower.util.DescData;
+import lea.playerknower.util.IEntityDataSaver;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.hit.HitResult;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
@@ -22,7 +27,15 @@ public class KeyInputHandler {
             if (displayKey.wasPressed()) {
                 // This happens when our custom key is pressed
                 //client.player.sendMessage(Text.of("Hello I pressed a Key"));
-                ClientPlayNetworking.send(ModMessages.DISPLAY_ID, PacketByteBufs.create());
+                //ClientPlayNetworking.send(ModMessages.DISPLAY_ID, PacketByteBufs.create());
+                if (!(client.crosshairTarget.getType() == HitResult.Type.ENTITY)) {
+                    return;
+                }
+                else {
+                    if (!(DescData.readShortDesc((IEntityDataSaver) client.crosshairTarget) == "" || DescData.readShortDesc((IEntityDataSaver) client.crosshairTarget) == null )) {
+                        client.player.sendMessage(Text.of(DescData.readShortDesc((IEntityDataSaver) client.crosshairTarget)));
+                    }
+                }
             }
         });
     }
